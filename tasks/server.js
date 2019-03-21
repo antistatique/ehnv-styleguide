df309ae72c2d9556e71e857184dac056616300ea
Yann Gouffon <github@yago-art.ch>
2019-03-21 11:57:05 +0100

    💄 add background-box molecule

--- tasks/server.js
+++ tasks/server.js
@@ -1,24 +1,23 @@
 'use strict';
 
-var gulp          = require('gulp'),
-    $             = require('gulp-load-plugins')(),
-    config        = require('../gulp_config.json'),
-    browserSync   = require('browser-sync'),
-    runSequence   = require('run-sequence');
+var gulp = require('gulp'),
+  $ = require('gulp-load-plugins')(),
+  config = require('../gulp_config.json'),
+  browserSync = require('browser-sync'),
+  runSequence = require('run-sequence');
 
 module.exports = function() {
-
   var reload = browserSync.reload;
 
- /**
-  * Serve
-  */
-  gulp.task('serve', ['default'], function () {
+  /**
+   * Serve
+   */
+  gulp.task('serve', ['default'], function() {
     browserSync({
       server: {
         baseDir: [config.app.basedir],
       },
-      open: false
+      open: false,
     });
     gulp.watch([config.assets + 'sass/**/*.scss'], function() {
       runSequence('styles', 'styleguide', reload);
@@ -35,14 +34,16 @@ module.exports = function() {
     gulp.watch([config.assets + 'js/**/*.js'], function() {
       runSequence('scripts', 'styleguide', reload);
     });
-    gulp.watch([
-      config.assets + 'components/**/*.{html,hbs,md}',
-      config.assets + 'templates/**/*.html',
-      config.assets + 'docs/**/*.md',
-      config.assets + 'data/**/*.{json,yml}'
-    ], function() {
-      runSequence('styleguide', reload);
-    });
+    gulp.watch(
+      [
+        config.assets + 'components/**/*.{html,hbs,md}',
+        config.assets + 'templates/**/*.html',
+        config.assets + 'docs/**/*.md',
+        config.assets + 'data/**/*.{json,yml}',
+      ],
+      function() {
+        runSequence('styleguide', reload);
+      },
+    );
   });
-
-}
+};
